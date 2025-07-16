d467bdc8f5823bd14b2b6a2de7d031534202bf15
Jana Enigma Baruc <barucjana@gmail.com>
2025-07-16 16:10:03 +0800

    added: challenge-lab2 subfolder

--- session1/challenge-lab2/src/test/java/com/example/challenge_lab2/ChallengeLab2ApplicationTests.java
+++ session1/challenge-lab2/src/test/java/com/example/challenge_lab2/ChallengeLab2ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.myfirstapi;
+package com.example.challenge_lab2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MyfirstapiApplicationTests {
+class ChallengeLab2ApplicationTests {
 
 	@Test
 	void contextLoads() {
