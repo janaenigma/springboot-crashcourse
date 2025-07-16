84fa781907407daea550a1ec4539443eb12f1f05
Jana Enigma Baruc <barucjana@gmail.com>
2025-07-16 15:57:32 +0800

    completed: session 1 - challenge-lab1

---
+++ session1/challenge-lab1/src/main/java/com/example/challenge_lab1/controller/HelloWorldController.java
@@ -0,0 +1,23 @@
+package com.example.challenge_lab1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+
+@RestController
+public class HelloWorldController {
+
+    @Value("${app.greeting.message}")
+    private String customGreeting;
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return customGreeting;
+    }
+
+    @GetMapping("/greet/{name}")
+    public String greetUser(@PathVariable String name) {
+        return "Greetings, " + name + "!";
+    }
+}
