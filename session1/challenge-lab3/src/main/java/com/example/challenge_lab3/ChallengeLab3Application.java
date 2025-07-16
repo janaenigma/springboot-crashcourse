33d3f6899999ac0c988cba9dc9c66847e8b4a075
Jana Enigma Baruc <barucjana@gmail.com>
2025-07-16 16:43:06 +0800

    completed: session 1 - challenge-lab3

---
+++ session1/challenge-lab3/src/main/java/com/example/challenge_lab3/ChallengeLab3Application.java
@@ -0,0 +1,13 @@
+package com.example.challenge_lab3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeLab3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeLab3Application.class, args);
+	}
+
+}
