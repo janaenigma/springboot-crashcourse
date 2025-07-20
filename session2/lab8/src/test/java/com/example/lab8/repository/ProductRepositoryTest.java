2c9fed7f853931f4b26fbc71bd6b475c075ba11b
Jana Enigma Baruc <barucjana@gmail.com>
2025-07-20 13:12:08 +0800

    add: initial files

---
+++ session2/lab8/src/test/java/com/example/lab8/repository/ProductRepositoryTest.java
@@ -0,0 +1,54 @@
+package com.example.lab8.repository;
+
+import com.example.lab8.model.Product;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductRepositoryTest {
+    ProductRepository productRepository;
+
+    @BeforeEach
+    void setup(){
+        productRepository = new ProductRepository();
+    }
+
+    @Test
+    void shouldFindAllProducts() {
+        List<Product> products = productRepository.findAll();
+        assertEquals(3, products.size());
+    }
+
+    @Test
+    void shouldReturnOptionalIfFindingExistingId() {
+        Optional<Product> result = productRepository.findById(1L);
+        assertTrue(result.isPresent());
+    }
+
+    @Test
+    void shouldReturnEmptyIfFindingNonExistentId() {
+        Optional<Product> result = productRepository.findById(4L);
+        assertTrue(result.isEmpty());
+    }
+
+    @Test
+    void shouldSaveNewProductIfProductHasNoId() {
+        Product newProduct = new Product(null, "Monitor", 100.00);
+        Product savedProduct = productRepository.save(newProduct);
+
+        assertNotNull(savedProduct.getId());
+        assertEquals("Monitor", savedProduct.getName());
+    }
+
+    @Test
+    void shouldUpdateProductIfProductHasAnId() {
+        Product updatedProduct = new Product(2L, "Wireless Mouse", 30.00);
+        Product savedProduct = productRepository.save(updatedProduct);
+
+        assertEquals(2L, savedProduct.getId());
+    }
+}
