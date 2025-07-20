5fe135d8da201d100928e4e68cb21a45259e6b43
Jana Enigma Baruc <barucjana@gmail.com>
2025-07-20 13:20:17 +0800

    complete: lab8: create GET All Products endpoint

---
+++ session2/lab8/src/main/java/com/example/lab8/controller/ProductController.java
@@ -0,0 +1,23 @@
+package com.example.lab8.controller;
+
+import com.example.lab8.model.Product;
+import com.example.lab8.repository.ProductRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ProductController {
+
+    private final ProductRepository productRepository;
+
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @GetMapping("/products")
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+}
